5e129d2938cd1937cafe7c41d0f7c6754f384394
Sergii Vorobei <sergeii.vorobei@yara.com>
2018-11-09 07:38:55 +0100

    refactroring calls

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# babel-plugin-bonify-rocks
+
+Babel plugin which transforms the `console.log(n)` calls to `console.log("Bonify rocks" + n)`.
+
+## Improvements:
+
+* Add support for `call`, `apply`
+* Add support for `bind`
--- src/index.js
+++ src/index.js
@@ -2,51 +2,23 @@
 
 module.exports = function ({ types: t }) {
     return {
-        name: "cool-bonify",
+        name: "babel-plugin-bonify-rocks",
         visitor: {
             CallExpression(path, state) {
                 const callee = path.get("callee");
 
                 if (!callee.isMemberExpression()) return;
 
-                if (isIncludedConsole(callee, state.opts.exclude)) {
-                
-
-                    // console.log()
-                    // if (path.parentPath.isExpressionStatement()) {
-                        // path.remove();
-                    // } else {
-                        // path.replaceWith(createVoid0());
-                    // }
+                if (isConsole(callee.get("object"))) {
                     const prepend = t.stringLiteral("Bonify rocks");
                     const expression = t.binaryExpression('+', prepend, path.get('arguments')[0].node);
                     path.node.arguments[0] = expression;
-                } else if (isIncludedConsoleBind(callee, state.opts.exclude)) {
-                    // console.log.bind()
-                    path.replaceWith(createNoop());
                 }
-            },
-            // MemberExpression: {
-            //     exit(path, state) {
-            //         if (
-            //             isIncludedConsole(path, state.opts.exclude) &&
-            //             !path.parentPath.isMemberExpression()
-            //         ) {
-            //             if (
-            //                 path.parentPath.isAssignmentExpression() &&
-            //                 path.parentKey === "left"
-            //             ) {
-            //                 path.parentPath.get("right").replaceWith(createNoop());
-            //             } else {
-            //                 path.replaceWith(createNoop());
-            //             }
-            //         }
-            //     }
-            // }
+            }
         }
     };
 
-    function isGlobalConsoleId(id) {
+    function isConsole(id) {
         const name = "console";
         return (
             id.isIdentifier({ name }) &&
@@ -55,43 +27,4 @@ module.exports = function ({ types: t }) {
         );
     }
 
-    function isExcluded(property, excludeArray) {
-        return (
-            excludeArray && excludeArray.some(name => property.isIdentifier({ name }))
-        );
-    }
-
-    function isIncludedConsole(memberExpr, excludeArray) {
-        const object = memberExpr.get("object");
-        const property = memberExpr.get("property");
-
-        if (isExcluded(property, excludeArray)) return false;
-        if (isGlobalConsoleId(object)) return true;
-
-        return (
-            isGlobalConsoleId(object.get("object")) &&
-            (property.isIdentifier({ name: "call" }) ||
-                property.isIdentifier({ name: "apply" }))
-        );
-    }
-
-    function isIncludedConsoleBind(memberExpr, excludeArray) {
-        const object = memberExpr.get("object");
-
-        if (!object.isMemberExpression()) return false;
-        if (isExcluded(object.get("property"), excludeArray)) return false;
-
-        return (
-            isGlobalConsoleId(object.get("object")) &&
-            memberExpr.get("property").isIdentifier({ name: "bind" })
-        );
-    }
-
-    function createNoop() {
-        return t.functionExpression(null, [], t.blockStatement([]));
-    }
-
-    function createVoid0() {
-        return t.unaryExpression("void", t.numericLiteral(0));
-    }
 };
